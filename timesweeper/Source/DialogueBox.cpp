5373aac3038feb2c5d4e6c39568d934e85bfc343
Jovan Djordjevic <joca.dj@hotmail.com>
2020-11-30 13:16:52 +0100

    feature: dialogue box promena
    sada ce se prikazivati drugaciji dialogue box u zavisnosti da li recenicu izgovara igrac ili NPC

--- timesweeper/Source/DialogueBox.cpp
+++ timesweeper/Source/DialogueBox.cpp
@@ -6,11 +6,22 @@ extern Game *game;
 DialogueBox::DialogueBox(QPair<DialogueHandler::Speaker, QString> &text)
 {
     //TODO: napraviti da drugaciji dialog box bude u zavisnosti od DialogueHandler::Speaker
-    setPixmap(QPixmap(":/Other/Resources/Other/dialogue_box.png"));
     currentText = new QGraphicsTextItem(this);
     currentText->setPlainText(text.second);
-    currentText->setTextWidth(300);
-    currentText->setPos(5,5);  //pozicija od gornjeg levog ugla pixmapa
+
+    if(text.first == DialogueHandler::Speaker::Strauss)
+    {
+        setPixmap(QPixmap(":/Other/Resources/Other/dialogue_box_strauss.png"));
+        currentText->setTextWidth(280);
+        currentText->setPos(10,20);   //pozicija od gornjeg levog ugla pixmapa
+    }
+    else
+    {
+        setPixmap(QPixmap(":/Other/Resources/Other/dialogue_box.png"));
+        currentText->setTextWidth(300);
+        currentText->setPos(5,5);
+    }
+
     QPointF playerPosition = game->player->pos();
     setPos(playerPosition + QPointF(50, -100));
 }
