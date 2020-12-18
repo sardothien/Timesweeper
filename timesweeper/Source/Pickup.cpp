8bd46872069dc4bead214b6ccadfc28cfbb97a8a
Igor Scekic <igorscekic98@gmail.com>
2020-12-18 16:09:59 +0100

    Dodat novi izgled za health u IV nivou

--- timesweeper/Source/Pickup.cpp
+++ timesweeper/Source/Pickup.cpp
@@ -9,6 +9,8 @@ Pickup::Pickup(QGraphicsPixmapItem *parent)
 {
     if(game->levelID == 2)
         setPixmap(QPixmap(":/Other/Resources/Other/health_level_2.png"));
+    else if(game->levelID == 4)
+        setPixmap(QPixmap(":/Other/Resources/Other/health_level_4.png"));
     else
         setPixmap(QPixmap(":/Images/Resources/testpickup.png"));
 }
